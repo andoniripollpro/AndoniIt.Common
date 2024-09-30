0fc4c621ccd346cb7d8da672378c40b8c77e7a55
ANDONI RIPOLL JARAUTA <andoni.ripolljarauta.ext@telefonica.com>
2024-09-30 18:50:37 +0200

    Oracle

--- AndoIt.Common/Common/OracleConnectionExtension.cs
+++ AndoIt.Common/Common/OracleConnectionExtension.cs
@@ -8,29 +8,41 @@ namespace AndoIt.Common.Common
 	public static class OracleConnectionExtension
 	{
 		public static void EnsureDatabaseConnection(this OracleConnection oracleConnection)
-		{
-			var log = IoCObjectContainer.Singleton.Get<ILog>();
-			try
-			{
-				new Insister(log).Insist(new Action(() => oracleConnection.OpenIfNeeded()), 5);
-			}
-			catch (Exception e)
-			{
-				log.Warn("EnsureDatabaseConnection. Connection to database lost.", e);
-			}
-		}
+        {
+            ILog log = GetLog();
+            try
+            {
+                new Insister(log).Insist(new Action(() => oracleConnection.OpenIfNeeded()), 5);
+            }
+            catch (Exception e)
+            {
+                log?.Warn("EnsureDatabaseConnection. Connection to database lost.", e);
+            }
+        }
 
-		public static void OpenIfNeeded(this OracleConnection oracleConnection)
+        private static ILog GetLog()
+        {
+            ILog log = null;
+            try
+            {
+                log = IoCObjectContainer.Singleton.Get<ILog>();
+            }
+            catch { Console.WriteLine("WARNING: Log estándar AndoIt no asignado"); }
+
+            return log;
+        }
+
+        public static void OpenIfNeeded(this OracleConnection oracleConnection)
 		{
-			var log = IoCObjectContainer.Singleton.Get<ILog>();
-			if (oracleConnection.State != ConnectionState.Open)
+            var log = GetLog();
+            if (oracleConnection.State != ConnectionState.Open)
 			{
 				oracleConnection.Open();
-				log.Debug("Connection.Open();");
+				log?.Debug("Connection.Open();");
 			}
 			else
 			{
-				log.Debug("Connection already openned");				
+				log?.Debug("Connection already openned");				
 			}
 		}
 	}

8037b87461d2257807ecc14dfe80ab28156c250e
ANDONI RIPOLL JARAUTA <andoni.ripolljarauta.ext@telefonica.com>
2025-02-24 16:40:46 +0100

    Error cuando la lista de elementos a eliminar estaba vacía

--- AndoIt.Common/Common/RabbitMQCreator.cs
+++ AndoIt.Common/Common/RabbitMQCreator.cs
@@ -1,6 +1,7 @@
 ﻿using AndoIt.Common.Interface;
 using RabbitMQ.Client;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace AndoIt.Common.Common
@@ -14,46 +15,57 @@ namespace AndoIt.Common.Common
 			this.log = log ?? throw new ArgumentNullException(nameof(log));
 		}
 
-		public void CreateExchangeQueueAndLinkIfPossible(string exchangeQueueLinkName, IModel channel, string routingKey)
-		{
-			this.log.Debug($"Start", new StackTrace(), exchangeQueueLinkName, channel, routingKey);
-			//Exchange
-			try
-			{
-				//type: ExchangeType.Direct
-				channel.ExchangeDeclare(exchange: exchangeQueueLinkName,
-					type: ExchangeType.Fanout,
-					durable: true,
-					autoDelete: false,
-					arguments: null);
-			}
-			catch (Exception ex)
-			{
-				this.log.Warn($"Al crear el exchange homonima al queue: '{exchangeQueueLinkName}'", ex);				
-			}
-			//Queue
-			try
-			{
-				channel.QueueDeclare(queue: exchangeQueueLinkName,
-					durable: true,
-					exclusive: false,
-					autoDelete: false,
-					arguments: null);
-			}
-			catch (Exception ex)
-			{
-				this.log.Warn($"Al crear una queue homonima al exchange: '{exchangeQueueLinkName}'", ex);				
-			}
-			//Exchange-Queue Link
-			try
-			{
-				channel.QueueBind(exchangeQueueLinkName, exchangeQueueLinkName, routingKey);
-			}
-			catch (Exception ex)
-			{
-				this.log.Warn($"Al lincar la queue y la exchange: '{exchangeQueueLinkName}'", ex);
-			}
-			this.log.Info($"End", new StackTrace());
-		}
-	}
+        public void CreateExchangeQueueAndLinkIfPossible(string exchangeQueueLinkName, IModel channel, string routingKey)
+        {
+            this.log.Debug($"Start", new StackTrace(), exchangeQueueLinkName, channel, routingKey);
+
+            // Exchange
+            try
+            {
+                channel.ExchangeDeclare(
+                    exchange: exchangeQueueLinkName,
+                    type: ExchangeType.Fanout, // Se mantiene Fanout, cambiar si es necesario
+                    durable: true,
+                    autoDelete: false,
+                    arguments: null);
+            }
+            catch (Exception ex)
+            {
+                this.log.Warn($"Al crear el exchange homónimo al queue: '{exchangeQueueLinkName}'", ex);
+            }
+
+            // Queue en modo Quorum
+            try
+            {
+                var queueArguments = new Dictionary<string, object>
+                {
+                    { "x-queue-type", "quorum" } // 🚀 Configuración clave para que la cola sea de tipo Quorum
+                };
+
+                channel.QueueDeclare(
+                    queue: exchangeQueueLinkName,
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: queueArguments);
+            }
+            catch (Exception ex)
+            {
+                this.log.Warn($"Al crear una queue homónima al exchange en modo Quorum: '{exchangeQueueLinkName}'", ex);
+            }
+
+            // Vinculación Exchange-Queue
+            try
+            {
+                channel.QueueBind(exchangeQueueLinkName, exchangeQueueLinkName, routingKey);
+            }
+            catch (Exception ex)
+            {
+                this.log.Warn($"Al vincular la queue y el exchange: '{exchangeQueueLinkName}'", ex);
+            }
+
+            this.log.Info($"End", new StackTrace());
+        }
+
+    }
 }
